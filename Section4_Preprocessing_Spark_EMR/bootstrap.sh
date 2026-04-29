be869250f63609456ee3c8dfd204d92046753053
Ahmed Al-shobaki <71884822+ahmedsh711@users.noreply.github.com>
2026-04-29 19:50:46 +0300

    Update bootstrap.sh

--- Section4_Preprocessing_Spark_EMR/bootstrap.sh
+++ Section4_Preprocessing_Spark_EMR/bootstrap.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo pip3 install --upgrade pip
-sudo pip3 install matplotlib numpy pandas pyarrow fsspec s3fs
+sudo pip3 install matplotlib numpy pandas pyarrow fsspec s3fs boto3
 sudo pip3 install datasets --ignore-installed --no-deps
 sudo pip3 install huggingface-hub tqdm requests filelock --ignore-installed
